f844ff378363097af0cb50cbbce629d77e769c29
xR3l4Xx <halloka.mangoka@gmail.com>
2022-09-17 16:36:08 +0300

    Read location values

--- app/src/main/java/com/example/sbma_locationmap/MainActivity.kt
+++ app/src/main/java/com/example/sbma_locationmap/MainActivity.kt
@@ -1,22 +1,59 @@
 package com.example.sbma_locationmap
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.sbma_locationmap.ui.theme.SBMA_LocationMapTheme
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.*
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var fusedLocatinoClient: FusedLocationProviderClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        requestPermission()
+
         setContent {
             Text("Location Map Lab")
         }
+
+        fusedLocatinoClient = LocationServices.getFusedLocationProviderClient(this)
+        if(ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            fusedLocatinoClient.lastLocation.addOnSuccessListener {
+                Log.i("pengb", "latitude: ${it.latitude}, longitude: ${it.longitude}, etc: $it")
+            }
+        }
+
+        val locationCallback = object: LocationCallback(){
+            override fun onLocationResult(p0: LocationResult) {
+                for(location in p0.locations){
+                    Log.d("pengb",
+                        "latitude: ${location.latitude} longitude: ${location.longitude} etc: $location")
+                }
+            }
+        }
+
+        val locationRequest = LocationRequest
+            .create()
+            .setInterval(1000)
+            .setPriority(Priority.PRIORITY_HIGH_ACCURACY)
+        fusedLocatinoClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
+
+    }
+
+
+    private fun requestPermission(){
+        ActivityCompat.requestPermissions(
+            this, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION), 100
+        )
     }
 }
